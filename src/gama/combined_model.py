77596cd9570b1aacbfee0409afdc5636f9c40438
jeremyzarge <jz642@cornell.edu>
2025-02-06 16:59:30 -0500

    Adding annotations

--- src/gama/combined_model.py
+++ src/gama/combined_model.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from model_a import ModelA
 from model_b import ModelB
 from model_c import ModelC
@@ -5,6 +6,7 @@ from helpers import *
 from state import GameState
 import numpy as np
 import copy
+
 
 class CombinedModel:
     def __init__(self):
@@ -18,25 +20,28 @@ class CombinedModel:
     #     completes_s = np.array([c for c in completes_set])
     #     completes_str = np.array([c for c in completes_straight])
     #     return np.concatenate(np.array([our_hand_value, turn, other_player_num_cards]), np.concatenate(np.concatenate(completes_s, completes_str), last))
-    
+
     def features(self, state: GameState):
         our_hand_value, _, other_player_num_cards, turn, last_five, completes_set, completes_straight = state.get_features()
         last = np.array(last_five)
         completes_s = np.array(completes_set)
         completes_str = np.array(completes_straight)
-        basic_features = np.array([our_hand_value, turn, other_player_num_cards])
+        basic_features = np.array(
+            [our_hand_value, turn, other_player_num_cards])
         return np.concatenate([basic_features, completes_s, completes_str, last])
 
-    def game_iteration(self, state : GameState):
+    def game_iteration(self, state: GameState):
         features = self.features(state)
         move1, move2, move3 = self.choose_actions(features, state)
-    
-    def choose_actions(self, features : np.ndarray[int], state : GameState) -> tuple:
-        first_moves = ["Continue"] + (["Yaniv"] if state.can_yaniv(state.player_1_hand) else [])
+
+    def choose_actions(self, features: np.ndarray[int], state: GameState) -> tuple:
+        first_moves = ["Continue"] + \
+            (["Yaniv"] if state.can_yaniv(state.player_1_hand) else [])
         move1 = self.m1.choose_action(first_moves, features)
-        
+
         second_moves = state.valid_move_values(state.player_1_hand)
-        move_value = self.m2.choose_action([i[0] for i in second_moves], features)
+        move_value = self.m2.choose_action(
+            [i[0] for i in second_moves], features)
         for i, move in second_moves:
             if i == move_value:
                 move2 = (i, move)
@@ -45,30 +50,33 @@ class CombinedModel:
         move3 = self.m3.choose_action(third_moves, features)
 
         return move1, move2, move3, first_moves, second_moves, third_moves
-    
-    def play_step(self, state : GameState, actions : tuple[str, list[int], str]) -> tuple[GameState, int, bool, bool]:
+
+    def play_step(self, state: GameState, actions: tuple[str, list[int], str]) -> tuple[GameState, int, bool, bool]:
         move1, move2, move3 = actions
         done = False
         win = False
         if move1 == "Yaniv":
             done = True
             win = state.yaniv(state.player_1_hand, [state.player_2_hand])
-            reward = state.get_hand_value(state.player_1_hand) * 5 if win else -5
+            reward = state.get_hand_value(
+                state.player_1_hand) * 5 if win else -5
         draw = -1 if move3 == "Deck" else 0 if move3 == "Draw 0" else 1
         initial_hand = state.get_hand_value(state.player_1_hand)
         state.play(state.player_1_hand, move2[1], draw)
         reward = initial_hand - state.get_hand_value(state.player_1_hand)
         state.playOpponentTurn()
-        
+
         return state, reward, done, win
-    
-    def update_weights(self, features : np.ndarray[int], actions : tuple[str, int, str], 
-        possible_actions : tuple[list], reward : int) -> None:
+
+    def update_weights(self, features: np.ndarray[int], actions: tuple[str, int, str],
+                       possible_actions: tuple[list], reward: int) -> None:
         move1, move2, move3 = actions
         poss1, poss2, poss3 = possible_actions
         self.m1.update_weights(move1, poss1, reward, features)
-        self.m2.update_weights(move2[0], [p[0] for p in poss2], reward, features)
+        self.m2.update_weights(move2[0], [p[0]
+                               for p in poss2], reward, features)
         self.m3.update_weights(move3, poss3, reward, features)
+
 
 def main():
     won_games = 0
@@ -89,6 +97,7 @@ def main():
             # print(state.player_1_hand)
         won_games += 1 if win else 0
     print(won_games)
-        
+
+
 if __name__ == "__main__":
     main()
--- src/gama/state.py
+++ src/gama/state.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 import numpy as np
 import helpers
+
 
 class GameState:
     def __init__(self) -> None:
@@ -17,21 +19,21 @@ class GameState:
         self.cards_played = []
         self.over = False
 
-    def valid_move_values(self, hand : np.ndarray[int]) -> list[tuple[int, list[int]]]:
+    def valid_move_values(self, hand: np.ndarray[int]) -> list[tuple[int, list[int]]]:
         valid_moves = []
         nonzeros = np.nonzero(hand)
         for i in nonzeros[0]:
             x = self.move_value(hand, i)
             valid_moves += self.move_value(hand, i)
         return valid_moves
-    
+
     def valid_third_moves(self) -> list[str]:
         valid_moves = ["Deck", "Card 0"]
         if len(self.top_cards) == 2:
             valid_moves.append("Card 1")
         return valid_moves
 
-    def move_value(self, hand : np.ndarray[int], card : int) -> list[tuple[int, list[int]]]:
+    def move_value(self, hand: np.ndarray[int], card: int) -> list[tuple[int, list[int]]]:
         suit = card // 13
         rank = card % 13
 
@@ -41,9 +43,11 @@ class GameState:
 
         hand_suit = hand_copy[suit]
         value_tups = []
-        after = (hand_suit[(rank + 1) % 13] == 1 and hand_suit[(rank + 2) % 13] == 1)
+        after = (hand_suit[(rank + 1) % 13] ==
+                 1 and hand_suit[(rank + 2) % 13] == 1)
         if after:
-            after_value, indices = min((rank + 1) % 13, 10) + min((rank + 2) % 13, 10) + min((rank + 3) % 13, 10), (rank, rank+1, rank+2)
+            after_value, indices = min((rank + 1) % 13, 10) + min(
+                (rank + 2) % 13, 10) + min((rank + 3) % 13, 10), (rank, rank+1, rank+2)
             value_tups.append((after_value, indices))
 
         for set_comb in set_combinations:
@@ -52,23 +56,26 @@ class GameState:
                 value_tups.append((min((rank + 1), 10) * length, set_comb))
         return value_tups
 
-    def card_indices(self, hand : np.ndarray[int]) -> list[int]:
+    def card_indices(self, hand: np.ndarray[int]) -> list[int]:
         return np.nonzero(hand)
 
-    def completes_set(self, hand : np.ndarray[int], card : int) -> bool:
+    def completes_set(self, hand: np.ndarray[int], card: int) -> bool:
         hand_copy = hand.copy()
         hand_copy[card] += 1
         hand_copy.reshape((4, 13)).clip(0, 1)
         return len(np.nonzero(hand_copy.reshape((52)))) > 1
-    
-    def completes_straight(self, hand : np.ndarray[int], card : int) -> bool:
+
+    def completes_straight(self, hand: np.ndarray[int], card: int) -> bool:
         suit = card // 13
         rank = card % 13
         hand_reshaped = hand.copy().reshape((4, 13))
         hand_suit = hand_reshaped[suit]
-        between = (hand_suit[(rank - 1) % 13] == 1 and hand_suit[(rank + 1) % 13] == 1)
-        before = (hand_suit[(rank - 1) % 13] == 1 and hand_suit[(rank - 2) % 13] == 1)
-        after = (hand_suit[(rank + 1) % 13] == 1 and hand_suit[(rank + 2) % 13] == 1)
+        between = (hand_suit[(rank - 1) % 13] ==
+                   1 and hand_suit[(rank + 1) % 13] == 1)
+        before = (hand_suit[(rank - 1) % 13] ==
+                  1 and hand_suit[(rank - 2) % 13] == 1)
+        after = (hand_suit[(rank + 1) % 13] ==
+                 1 and hand_suit[(rank + 2) % 13] == 1)
         return between or before or after
 
     def get_features(self) -> tuple[int, list[int], int, int, list[int], bool, bool]:
@@ -78,11 +85,13 @@ class GameState:
         turn = self.turn
         last_five = self.cards_played[-5:]
         top_cards = self.top_cards
-        completes_set = [self.completes_set(self.player_1_hand, card) for card in top_cards]
-        completes_straight = [self.completes_straight(self.player_1_hand, card) for card in top_cards]
+        completes_set = [self.completes_set(
+            self.player_1_hand, card) for card in top_cards]
+        completes_straight = [self.completes_straight(
+            self.player_1_hand, card) for card in top_cards]
         return our_hand_value, our_cards, other_player_num_cards, turn, last_five, completes_set, completes_straight
 
-    def get_hand_value(self, hand : np.ndarray[int]) -> int:
+    def get_hand_value(self, hand: np.ndarray[int]) -> int:
         sum = (hand.reshape((4, 13)) * np.arange(1, 14)).clip(0, 10).sum()
         return sum
 
@@ -95,26 +104,26 @@ class GameState:
         self.top_cards = []
         self.curr_idx += 1
         return self.deck[self.curr_idx]
-    
-    def can_yaniv(self, hand : np.ndarray) -> bool:
+
+    def can_yaniv(self, hand: np.ndarray) -> bool:
         return self.get_hand_value(hand) <= 7
-    
-    def yaniv(self, hand : np.ndarray[int], other_hands : list[np.ndarray]) -> bool:
+
+    def yaniv(self, hand: np.ndarray[int], other_hands: list[np.ndarray]) -> bool:
         our_value = self.get_hand_value(hand)
         for others in other_hands:
             if our_value <= self.get_hand_value(others):
                 return False
         return True
-    
-    def draw(self, idx : int) -> int:
+
+    def draw(self, idx: int) -> int:
         if idx <= len(self.top_cards) - 1:
             card = self.top_cards[idx]
         else:
             card = self.top_cards[0]
         self.top_cards = []
         return card
-    
-    def play(self, hand : np.ndarray[int], cards : list[int], draw_idx : int) -> int:
+
+    def play(self, hand: np.ndarray[int], cards: list[int], draw_idx: int) -> int:
         print(f"Hand s as {hand}")
         self.discard += self.top_cards
         if draw_idx >= 0:
@@ -131,7 +140,7 @@ class GameState:
         hand[card_drawn] += 1
         print(f"Hand e as {hand}")
         return card_drawn
-    
+
     def playOpponentTurn(self) -> tuple[bool, bool]:
         done = False
         won = False
@@ -140,7 +149,7 @@ class GameState:
             done = True
         else:
             moves = self.valid_move_values(self.player_2_hand)
-            moves.sort(key = lambda move : move[0])
+            moves.sort(key=lambda move: move[0])
             move_i = moves[0]
             curr = "Deck"
             drew = False
